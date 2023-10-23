5cbef4823793016e199c138f4d454d73a2849db0
lemunozm <lemunozm@gmail.com>
2023-10-23 19:42:26 +0200

    minor change

--- mock-builder/src/lib.rs
+++ mock-builder/src/lib.rs
@@ -57,7 +57,7 @@
 //! associated type in our `Config`, which implements traits `TraitA` and
 //! `TraitB`. Those traits are defined as follows:
 //!
-//! ```no_run
+//! ```
 //! trait TraitA {
 //!     type AssocA;
 //!
@@ -143,7 +143,7 @@
 //! The only condition to use these macros is to have the following storage in
 //! the pallet (it's safe to just copy and paste this snippet in your pallet):
 //!
-//! ```no_run
+//! ```
 //! # #[frame_support::pallet(dev_mode)]
 //! # mod pallet {
 //! # use frame_support::pallet_prelude::*;
@@ -153,7 +153,7 @@
 //! # pub struct Pallet<T>(_);
 //!
 //! #[pallet::storage]
-//! pub(super) type CallIds<T: Config> = StorageMap<_, _, String, mock_builder::CallId>;
+//! type CallIds<T: Config> = StorageMap<_, _, String, mock_builder::CallId>;
 //!
 //! # }
 //! ```
@@ -161,7 +161,7 @@
 //! Following the above example, generating a *mock pallet* for both `TraitA`
 //! and `TraitB` is done as follows:
 //!
-//! ```no_run
+//! ```
 //! #[frame_support::pallet(dev_mode)]
 //! pub mod pallet {
 //!     # trait TraitA {
@@ -189,7 +189,7 @@
 //!     pub struct Pallet<T>(_);
 //!
 //!     #[pallet::storage]
-//!     pub(super) type CallIds<T: Config> = StorageMap<_, _, String, mock_builder::CallId>;
+//!     type CallIds<T: Config> = StorageMap<_, _, String, mock_builder::CallId>;
 //!
 //!     impl<T: Config> Pallet<T> {
 //!         fn mock_foo(f: impl Fn() -> T::AssocA + 'static) {
