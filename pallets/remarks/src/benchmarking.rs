006791d758daedd56fa04095f2d993251347bb96
cdamian <17934949+cdamian@users.noreply.github.com>
2024-01-03 11:55:59 +0100

    pallet-remarks: Add linear arg to benchmarks

--- pallets/remarks/src/benchmarking.rs
+++ pallets/remarks/src/benchmarking.rs
@@ -28,11 +28,16 @@ fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 mod benchmarks {
 	use super::*;
 
+	/// The upper bound of the Linear param range should be
+	/// T::MaxRemarksPerCall, however, that type is not available there.
+	///
+	/// We are defaulting to 10 since that's the max number of remarks we will
+	/// use in the Centrifuge chain implementation.
 	#[benchmark]
-	fn remark() -> Result<(), BenchmarkError> {
+	fn remark(n: Linear<1, 10>) -> Result<(), BenchmarkError> {
 		let mut remarks = Vec::new();
 
-		for _ in 0..T::MaxRemarksPerCall::get() {
+		for _ in 0..n {
 			remarks.push(T::Remark::default())
 		}
 
@@ -59,34 +64,3 @@ mod benchmarks {
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Runtime);
 }
-
-// benchmarks! {
-// 	where_clause {
-// 		where
-// 			T::AccountId: EncodeLike<<T as frame_system::Config>::AccountId>,
-// 	}
-//
-// 	remark {
-// 		let c in 1 .. T::MaxRemarksPerCall::get();
-// 		let mut remarks = Vec::new();
-//
-// 		for i in 0 .. c {
-// 			remarks.push(T::Remark::default())
-// 		}
-//
-// 		let remarks = BoundedVec::<T::Remark,
-// T::MaxRemarksPerCall>::try_from(remarks).expect("can build remarks");
-//
-// 		let caller: T::AccountId = account("acc_0", 0, 0);
-// 		let call: <T as Config>::RuntimeCall = frame_system::Call::remark { remark:
-// vec![] }.into(); 	}: remark(RawOrigin::Signed(caller), remarks.clone(),
-// Box::new(call.clone())) 	verify {
-// 		assert_last_event::<T>(Event::Remark {
-// 			remarks,
-// 			call
-// 		}.into())
-// 	}
-// }
-//
-// impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(),
-// crate::mock::Runtime);
